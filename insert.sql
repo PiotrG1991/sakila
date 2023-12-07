18a804dda08b6b050372cc61952a6e07ad65d38c
Piotrek <piotrg3378@wp.pl>
2023-12-07 10:43:06 +0100

    piotr

---
+++ insert.sql
@@ -0,0 +1,38 @@
+
+
+CREATE TABLE if not exists actor_sample (
+    actor_id    SERIAL,
+    first_name  VARCHAR(45) NOT NULL,
+    last_name   VARCHAR(45) NOT NULL,
+    last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (actor_id)
+);
+
+INSERT INTO actor_sample(first_name, last_name) VALUES ('Jarosław', 'K');
+
+SELECT *
+FROM actor_sample;
+
+INSERT INTO actor_sample(first_name, last_name) VALUES ('Piotr', 'G');
+
+INSERT INTO actor_sample(first_name, last_name) VALUES ('Sławek', 'B');
+
+SELECT *
+FROM actor_sample;
+
+INSERT INTO actor_sample(first_name, last_name)
+VALUES ('Antoni', 'Macierewicz'),
+       ('Kamil', 'Kozłowski');
+
+INSERT INTO actor_sample (first_name, last_name)
+SELECT first_name, last_name
+FROM actor
+WHERE first_name = 'Nick';
+
+SELECT *
+FROM actor_sample;
+
+INSERT INTO actor_sample (first_name, last_name)
+SELECT first_name, last_name
+FROM actor
+WHERE first_name = 'KENETH';
